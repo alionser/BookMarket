0985f1eebbfd3357a12811282e9a5bf1bf460a7b
alionser <smirnoviksa@gmail.com>
2022-08-14 01:46:53 +0500

    Start BookMarket.Web application

---
+++ src/BookMarket.Web/Program.cs
@@ -0,0 +1,14 @@
+namespace BookMarket.Web;
+
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+}
---
+++ src/BookMarket.Web/Startup.cs
@@ -0,0 +1,27 @@
+namespace BookMarket.Web;
+
+public class Startup
+{
+    public IConfiguration Configuration;
+
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        
+        app.UseWelcomePage();
+    }
+}
